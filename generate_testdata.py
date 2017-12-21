187847a636e2e5d4a0710aea9057859e1ea5d38c
Simon Persson <simon@flaskpost.org>
2017-12-21 23:40:27 +0100

    Add scripts to help testing.

---
+++ generate_testdata.py
@@ -0,0 +1,23 @@
+import random
+import itertools
+
+def sql_generator(insert, vals):
+    yield insert
+    yield next(vals)
+    for v in vals:
+        yield ','
+        yield v
+    yield ';'
+
+
+deck_insert = sql_generator("insert into deck(owner, name) values ", 
+        ("('sp','"+str(i)+"')" for i in range(1,10000)))
+card_insert = sql_generator("insert into card(front,back,bucket,deck_owner, deck_name) values ",
+        (("('front" + str(i) + "', 'back3'," + str(random.randint(10000,1000000))+", 'sp',"
+            + str(random.randint(1,9999))+")" for i in range(0, 10000))))
+
+with open("testdata.sql", "w") as f:
+    f.writelines(itertools.chain(
+        ["drop role sp; insert into flash_card_user(name,pass) values('sp','1');"], 
+        deck_insert,
+        card_insert))
