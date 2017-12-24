c6c1e661ab448982e3de88a02bea96f9300bff83
Simon Persson <simon@flaskpost.org>
2017-12-24 12:36:20 +0100

    Fix next bucket logic.

--- schema/5-api.sql
+++ schema/5-api.sql
@@ -2,21 +2,36 @@ DROP ROLE IF EXISTS web_anon;
 DROP VIEW IF EXISTS next_bucket;
 DROP VIEW IF EXISTS next_card;
 
--- The bucket from which the next card should be pulled is the first bucket
--- with more cards in it than the square of the bucket number. That is, we
--- pull a card from bucket 3 once it has more than 9 cards in it.
-CREATE VIEW next_bucket AS
-SELECT deck_owner, deck_name, MAX(bucket) AS bucket 
-FROM bucket_count GROUP BY (deck_owner, deck_name);
+-- If there are many full buckets, we start from the largest bucket.
+CREATE VIEW full_bucket AS
+SELECT bc.deck_owner, bc.deck_name, MAX(bc.bucket) AS bucket
+FROM bucket_count bc
+WHERE POWER(bc.bucket,2)<bc.count
+GROUP BY(bc.deck_owner, bc.deck_name);
 
+-- If there is no full bucket, we start from the smallest bucket.
+CREATE VIEW smallest_bucket_with_no_full AS
+SELECT bc.deck_owner, bc.deck_name, MIN(bc.bucket) AS bucket
+FROM bucket_count bc
+LEFT JOIN full_bucket fbc ON fbc.deck_owner=bc.deck_owner AND fbc.deck_name=bc.deck_name
+WHERE fbc.deck_owner IS NULL
+GROUP BY(bc.deck_owner, bc.deck_name);
+
+CREATE VIEW next_bucket AS
+SELECT * FROM full_bucket 
+UNION ALL 
+SELECT * FROM smallest_bucket_with_no_full;
+		
 -- The next card is either the first card in the overflowing
 -- largest overflowing bucket, or a random card.
 CREATE VIEW next_card AS
-SELECT DISTINCT ON (c.deck_owner,c.deck_name) c.* FROM (
-	SELECT c.* FROM card c INNER JOIN next_bucket nb ON nb.deck_owner = c.deck_owner AND nb.deck_name = c.deck_name AND nb.bucket = c.bucket
-	UNION ALL
-	(SELECT * FROM card ORDER BY RANDOM())
-) as c;
+SELECT DISTINCT ON (c.deck_owner,c.deck_name) c.front,c.back,c.deck_owner,c.deck_name,c.bucket FROM (
+  (
+    (SELECT 1 AS sort_nr, c.* FROM card c INNER JOIN next_bucket nb ON nb.deck_owner = c.deck_owner AND nb.deck_name = c.deck_name AND nb.bucket = c.bucket order by c.front)
+    UNION ALL
+    (SELECT 2 AS sort_nr, c.* FROM card c ORDER BY (c.bucket,c.front))
+  ) ORDER BY sort_nr
+) as c where c.deck_owner = CURRENT_ROLE;
 
 -- Anon user can read all public decks.
 CREATE ROLE web_anon NOLOGIN;
